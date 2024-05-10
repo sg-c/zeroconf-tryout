2ade7f230c53f00392f991846d3df48f75f92f59
Saiguang Che <3857317+sg-c@users.noreply.github.com>
2024-05-10 15:33:05 +0800

    update addr

--- server.py
+++ server.py
@@ -5,11 +5,16 @@ host_name = socket.gethostname()
 srv_type = "_hffs._tcp.local."
 srv_name = host_name + "." + srv_type
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+addr, _ = sock.getsockname()
+
+print(addr)
+
 info = ServiceInfo(srv_type,
                    srv_name,
                    28888,
                    properties={"a": "b"},
-                   addresses=[socket.inet_aton("127.0.0.1")])
+                   addresses=[socket.inet_aton(addr)])
 
 zc = Zeroconf()
 zc.register_service(info)
